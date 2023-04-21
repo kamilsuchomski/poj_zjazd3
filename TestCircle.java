8760e88c9964f90fae1acce3769516683cf41156
Kamil Suchomski <53348850+kamilsuchomski@users.noreply.github.com>
2023-04-21 23:49:09 +0200

    Update TestCircle.java

--- TestCircle.java
+++ TestCircle.java
@@ -7,19 +7,19 @@ public class TestCircle {
         Circle circleObj4 = new Circle();
 
         System.out.println("circle1 has radius of " + circleObj1.getRadius() + " and area of " + circleObj1.getArea() + " and color(hex) " + Integer.toHexString((int)circleObj1.getColor()));
-        System.out.println(circleObj1.toString());
+        System.out.println(circleObj1);
 
         System.out.println("circle2 has radius of " + circleObj2.getRadius() + " and area of " + circleObj2.getArea() + " and color(hex) " + Integer.toHexString((int)circleObj2.getColor()));
-        System.out.println(circleObj2.toString());
+        System.out.println(circleObj2);
 
         System.out.println("circle3 has radius of " + circleObj3.getRadius() + " and area of " + circleObj3.getArea() + " and color(hex) " + Integer.toHexString((int)circleObj3.getColor()));
-        System.out.println(circleObj3.toString());
+        System.out.println(circleObj3);
 
         circleObj4.setRadius(26.35);
         circleObj4.setColor(0xFFFF00);
 
         System.out.println("circle4 has radius of " + circleObj4.getRadius() + " and area of " + circleObj4.getArea() + " and color(hex) " + Integer.toHexString((int)circleObj4.getColor()));
-        System.out.println(circleObj4.toString());
+        System.out.println(circleObj4);
 
     }
 }
